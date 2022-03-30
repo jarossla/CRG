4dee5819ed619eb1f56ed8e495d1603e25b89b95
jarossla <90608757+jarossla@users.noreply.github.com>
2022-03-30 18:34:52 +0200

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -4,6 +4,7 @@
 #include<windows.h>
 #include<time.h>
 
+//DEFINICIA VELKOSTI OBRAZOVKY , KTORU BUDEME POUZIVAT  NA VYTVORENIE HRY
 #define SCREEN_WIDTH 120
 #define SCREEN_HEIGHT 50
 #define WIN_WIDTH 90
@@ -16,9 +17,9 @@ COORD CursorPosition;
 //DEFINUJE SA ENEMY A JEHO VELKOST
 int enemyY[3];
 int enemyX[3];
-int enemyFlag[5];
+int enemyFlag[6];
 //VYKRESLENIE AUTA POMOCOU ZNAKOV
-char car[6][5] = {  ' ',' ','8',' ',' ',
+char car[6][5] = { ' ',' ','8',' ',' ',
 					'8','8','8','8','8',
 					'8','8','8','8','8',
 					' ','8','8','8',' ',
@@ -42,44 +43,45 @@ void setcursor(bool visible, DWORD size) {
 	lpCursor.dwSize = size;
 	SetConsoleCursorInfo(console, &lpCursor);
 }
-//NAKRESLI HRANICU ZA KTORU SA AUTOM NEMOZEME DOSTAT
+//NAKRESLI HRANICU ZA KTORU SA AUTOM NEMOZEME DOSTAT A ZA KTORU SA NEBUDU GENEROVAT ENEMY
 void drawBorder() {
 	for (int i = 0; i < SCREEN_HEIGHT; i++) {
-		for (int j = 0; j < 5; j++) {
+		for (int j =0 ; j <10 ; j++) {
 			gotoxy(0 + j, i); cout << "I";
 			gotoxy(WIN_WIDTH - j, i); cout << "I";
 		}
 	}
+	// ZNAKOVO URCUJE CELKOVU VELKOST DEFINOVANIE OBRAZOVKU
 	for (int i = 0; i < SCREEN_HEIGHT; i++) {
 		gotoxy(SCREEN_WIDTH, i); cout << "+";
-		gotoxy(SCREEN_WIDTH +1, i); cout << "+";
+		gotoxy(SCREEN_WIDTH + 1, i); cout << "+";
 
 	}
 }
 // URCUJE KDE SA MAJU GENEROVAT ENEMY
 void genEnemy(int ind) {
-	enemyX[ind] = 5 + rand() % (67);
-	enemyY[ind] = 6 + rand() % (67);
+	enemyX[ind] = 25 + rand() % (33);	
 }
 //VYKRESLI ENEMY POMOCOU ZNAKOV A URCI ICH POLOHU OD SEBA
 void drawEnemy(int ind) {
 
-	if (enemyFlag[ind] = true) {     // dopisat jedno rovnasa
+	if (enemyFlag[ind] = true) {     
 
-		gotoxy(enemyX[ind], enemyY[ind]); cout <<     "****";
-		gotoxy(enemyX[ind], enemyY[ind] +1); cout <<  " ** ";
-		gotoxy(enemyX[ind], enemyY[ind] +2); cout <<  "****";
-		gotoxy(enemyX[ind], enemyY[ind] +3); cout <<  " ** ";
+		gotoxy(enemyX[ind], enemyY[ind]);     cout << "******";
+		gotoxy(enemyX[ind], enemyY[ind] + 1); cout << " **** ";
+		gotoxy(enemyX[ind], enemyY[ind] + 2); cout << "******";
+		gotoxy(enemyX[ind], enemyY[ind] + 3); cout << " **** ";
 	}
 }
+//VYMAZE ENEMY
 void eraseEnemy(int ind) {
 
-	if (enemyFlag[ind] = true) { // dopisat jedno rovnasa
+	if (enemyFlag[ind] = true) { 
 
-		gotoxy(enemyX[ind], enemyY[ind]);     cout << "    ";
-		gotoxy(enemyX[ind], enemyY[ind] +1 ); cout << "    ";
-		gotoxy(enemyX[ind], enemyY[ind] +2); cout <<  "    ";
-		gotoxy(enemyX[ind], enemyY[ind] +3); cout <<  "    ";
+		gotoxy(enemyX[ind],enemyY[ind]);      cout << "      ";
+		gotoxy(enemyX[ind], enemyY[ind] + 1); cout << "    ";
+		gotoxy(enemyX[ind], enemyY[ind] + 2); cout << "      ";
+		gotoxy(enemyX[ind], enemyY[ind] + 3); cout << "    ";
 	}
 }
 void resetEnemy(int ind) {
@@ -98,6 +100,7 @@ void drawCar() {
 		}
 	}
 }
+// VYMAZE ZNAKY AUTA KED SA POHNEME NIM
 void eraseCar() {
 
 	for (int i = 0; i < 5; i++) {
@@ -106,11 +109,10 @@ void eraseCar() {
 		}
 	}
 }
-
+// URCUJE PRESNOST KOLIZIE AUTA S ENEMY
 int collision() {
-
-	if (enemyY[0] + 4 >= 45) {
-		if (enemyX[0] + 4 - carPos >= 0 && enemyX[0] + 4 - carPos < 22) {
+	if (enemyY[0] + 5 >= 40) {
+		if (enemyX[0] + 2 - carPos >= 0 && enemyX[0] + 2 - carPos < 5) {
 			return 1;
 		}
 	}
@@ -120,12 +122,13 @@ int collision() {
 void gameover() {
 	system("cls");
 
-	gotoxy(95, 20); cout << "\t\t--------------------------" << endl;
-	gotoxy(95, 22); cout << "\t\t-------- Prehral si ------" << endl;
-	gotoxy(95, 24); cout << "\t\t--------------------------" << endl;
-	gotoxy(95, 26); cout << "\t\tStlac hocico na klavesnici na vratenie sa do menu" << endl;
+	gotoxy(90, 20); cout << "-------------------------------------------------" << endl;
+	gotoxy(90, 22); cout << "------------------ Prehral si -------------------" << endl;
+	gotoxy(90, 24); cout << "-------------------------------------------------" << endl;
+	gotoxy(90, 26); cout << "Stlac hocico na klavesnici na vratenie sa do menu" << endl;
 	_getch();
 }
+// PO KAZDOM PRECHODE ENEMY PO CELEJ PLOCHE SA SKORE RESETUJE O +2
 void updateScore() {
 	gotoxy(WIN_WIDTH + 7, 5); cout << "Score: " << score << endl;
 }
@@ -142,9 +145,9 @@ void instrukcie() {
 	gotoxy(95, 27); cout << "Stlac hocico na klavesnici na vratenie do menu";
 	_getch();
 }
-
+//NASTAVIA SA FUNKCIE NA HRANIE HRY , KTORE SME SI PREDTYM VYTVORILI + SA VYPISE 
 void play() {
-	carPos = -1+ WIN_WIDTH / 2;
+	carPos = -1 + WIN_WIDTH / 2;
 	score = 0;
 	enemyFlag[0] = 1;
 	enemyFlag[1] = 0;
@@ -167,9 +170,9 @@ void play() {
 	gotoxy(18, 5); cout << "Stlac kocico na klavesnici na zacatie hry";
 	_getch();
 	gotoxy(18, 5); cout << "                                         ";
-	
-	
 
+
+	// URCUJE AKO DALEKO SA PUDEME POHYBOVAT OD STREDU DEFINOVENEJ OBRAZOVKY A AKYMI KLAVESNICAMI SA AUTO BUDE POHYBOVAT
 	char ch;
 	while (1) {
 		if (_kbhit()) {
@@ -180,7 +183,7 @@ void play() {
 					carPos -= 2;
 			}
 			if (ch == 'd' || ch == 'D') {
-				if (carPos < 60)
+				if (carPos < 70)
 					carPos += 2;
 			}
 			if (int(ch) == 27) {
@@ -231,17 +234,19 @@ int main()
 		system("cls");
 
 		//VYPISE SA MENU HRY
-		gotoxy(95, 15); cout << "000000000000000000000000000000"; 
-		gotoxy(95, 17); cout << "0{ <<<<<<<Car Game>>>>>>>>> }0";
-		gotoxy(95, 19); cout << "000000000000000000000000000000"; 
-		gotoxy(95, 21); cout << "Vyber jednu vec:			   ";
-		gotoxy(95, 23); cout << "1. Zacatie hry                ";
-		gotoxy(95, 25); cout << "2. Instrukcie				   ";
-		gotoxy(95, 27); cout << "3. Koniec					   ";
-		
+		gotoxy(95, 14); cout << "000000000000000000000000000000";
+		gotoxy(95, 15); cout << "000000000000000000000000000000";
+		gotoxy(95, 17); cout << "00<<<<<<< CAR GAME >>>>>>>>>00";
+		gotoxy(95, 19); cout << "000000000000000000000000000000";
+		gotoxy(95, 20); cout << "000000000000000000000000000000";
+		gotoxy(95, 22); cout << "Vyber jednu vec:			   ";
+		gotoxy(95, 24); cout << "1. Zacatie hry                ";
+		gotoxy(95, 26); cout << "2. Instrukcie				   ";
+		gotoxy(95, 28); cout << "3. Koniec					   ";
+
 
 		char op = _getch();
-
+		// URCUJE AKYMI KLAVESNICAMI SI MOZEME ZAPNUT OBSAH MENU
 		if (op == '1') play();
 		else if (op == '2') instrukcie();
 		else if (op == '3') exit(0);
